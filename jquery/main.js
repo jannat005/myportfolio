a85200bcd82003c98e178cf7636573e9576cca85
Jannatul Ferdaus <54632535+jannat005@users.noreply.github.com>
2019-09-09 13:57:12 +0600

    update main.js

--- jquery/main.js
+++ jquery/main.js
@@ -104,7 +104,6 @@ $("#navigation li a").click(function(e) {
     animation: 'fade', // Fade, slide, none
     animationInSpeed: 200, // Animation in speed (ms)
     animationOutSpeed: 200, // Animation out speed (ms)
-    scrollText: 'Scroll to top', // Text for element
     activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
   });
 });
