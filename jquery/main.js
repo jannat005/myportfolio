a665f515d5ea865e297a60e8b5d76388824e33b1
Jannatul Ferdaus <54632535+jannat005@users.noreply.github.com>
2019-09-09 13:39:20 +0600

    update main.js

--- jquery/main.js
+++ jquery/main.js
@@ -94,7 +94,8 @@ $("#navigation li a").click(function(e) {
     var targetPosition = $(targetElement).offset().top;
     $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");				 
         });
-        
+
+        $(function () {
         $.scrollUp({
     scrollName: 'scrollUp', // Element ID
     topDistance: '300', // Distance from top before showing element (px)
@@ -105,6 +106,6 @@ $("#navigation li a").click(function(e) {
     scrollText: 'Scroll to top', // Text for element
     activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
   }); 
-
+});
 
 		
