82fa038c7b0aef1157513b4c4442c7375dbfefc3
Jannatul Ferdaus <54632535+jannat005@users.noreply.github.com>
2019-09-09 13:41:38 +0600

    update main.js

--- jquery/main.js
+++ jquery/main.js
@@ -95,17 +95,18 @@ $("#navigation li a").click(function(e) {
     $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");				 
         });
 
+/*scrolltop*/
         $(function () {
-        $.scrollUp({
+  $.scrollUp({
     scrollName: 'scrollUp', // Element ID
     topDistance: '300', // Distance from top before showing element (px)
-    topSpeed: 500, // Speed back to top (ms)
+    topSpeed: 300, // Speed back to top (ms)
     animation: 'fade', // Fade, slide, none
     animationInSpeed: 200, // Animation in speed (ms)
     animationOutSpeed: 200, // Animation out speed (ms)
     scrollText: 'Scroll to top', // Text for element
     activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-  }); 
+  });
 });
 
 		
